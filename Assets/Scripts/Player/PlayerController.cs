56fb52afd86c5b9a3498df012f8c38b14634cc51
SaulCamachoD <114762933+SaulCamachoD@users.noreply.github.com>
2024-03-11 23:11:55 -0500

    Improve Movements and Animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 50f;
-    private float fwdSpeed = 2f;
+    public float speed = 180f;
+    public float rotation = 18f;
     private Rigidbody rb;
     public Animator animator;
     private int footLayerIndex;
     private int baseLayerIndex;
-    private bool isAttacking = false;
+    //private bool isAttacking = false;
     private Coroutine resetLayerWeightCoroutine;
     private bool attack1Pressed = false;
     private bool attack2Pressed = false;
@@ -23,8 +23,6 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        footLayerIndex = animator.GetLayerIndex("Foots");
-        baseLayerIndex = animator.GetLayerIndex("Base Layer");
     }
 
     private void Update()
@@ -36,67 +34,49 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!isAttacking)
+        
         {
+            float movsX = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(0f, 0f, movsX).normalized;
 
-            float movsX = Input.GetAxis("Horizontal");
-            float movsY = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(movsX, 0f, movsY).normalized;
             if (Input.GetKey(KeyCode.W))
             {
-                rb.velocity = movement * speed * fwdSpeed * Time.deltaTime;
-            }
-            else
-            {
+                // Calculamos la dirección del movimiento en función de la rotación actual
+                movement = transform.TransformDirection(movement);
                 rb.velocity = movement * speed * Time.deltaTime;
             }
-        }
 
-        else
-        {
-            rb.velocity = Vector3.zero;
-        }
+            // Calculamos la rotación basada en la entrada de teclado
+            float rotationAmount = 0f;
+            if (Input.GetKey(KeyCode.D))
+            {
+                rotationAmount = rotation * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                rotationAmount = -rotation * Time.deltaTime;
+            }
 
+            // Aplicamos la rotación si se está girando
+            if (rotationAmount != 0f)
+            {
+                Quaternion deltaRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+        }
+        
     }
 
     private void animationMovement()
     {
-        if (!Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            
-            animator.SetFloat("fwd", 0.0f);
+
+            animator.SetBool("Walk", true);
         }
         else
         {
-            animator.SetFloat("fwd", 1.0f);
-        }
-
-        if (!Input.GetKey(KeyCode.S))
-        {
-            animator.SetFloat("aft", 0.0f);
-        }
-        else
-        {
-            animator.SetFloat("aft", -1.0f);
-        }
-
-
-        if (!Input.GetKey(KeyCode.D))
-        {
-            animator.SetFloat("right", 0.0f);
-        }
-        else
-        {
-            animator.SetFloat("right", 1.0f);
-        }
-
-        if (!Input.GetKey(KeyCode.A))
-        {
-            animator.SetFloat("left", 0.0f);
-        }
-        else
-        {
-            animator.SetFloat("left", -1.0f);
+            animator.SetBool("Walk", false);
         }
     }
 
@@ -146,8 +126,8 @@ public class PlayerController : MonoBehaviour
 
     private void TriggerAttack(string triggerName)
     {   
-        isAttacking = true;
-        animator.SetLayerWeight(footLayerIndex, 0f);
+        //isAttacking = true;
+        //animator.SetLayerWeight(footLayerIndex, 0f);
         animator.SetTrigger(triggerName);
         resetLayerWeightCoroutine = StartCoroutine(ResetLayerWeight());
     }
@@ -158,8 +138,8 @@ public class PlayerController : MonoBehaviour
         BladeLeft.SetActive(true);
         BladeRight.SetActive(true);
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(baseLayerIndex).length); // Esperar hasta que la animación actual termine
-        animator.SetLayerWeight(footLayerIndex, 1f);
-        isAttacking = false;
+        //animator.SetLayerWeight(footLayerIndex, 1f);
+        //isAttacking = false;
         resetLayerWeightCoroutine = null;
         BladeLeft.SetActive(false);
         BladeRight.SetActive(false);
